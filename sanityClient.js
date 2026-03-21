fdf4f9b5974b3a469ccdb5d555b75fa47ad63047
Joseph Sluterbeck <josephsluterbeck@gmail.com>
2026-03-21 14:42:03 -0400

    thx cursor

--- sanityClient.js
+++ sanityClient.js
@@ -1,9 +1,8 @@
-// sanityClient.js
 import sanityClient from '@sanity/client'
 
 export const client = sanityClient({
-  projectId: 'YOUR_PROJECT_ID', // find in sanity.json or on sanity.io/manage
-  dataset: 'production',        // or your dataset name
-  apiVersion: '2026-03-21',     // use today's date
-  useCdn: true,                 // `false` if you want latest content always
+  projectId: 'orxxqp7k', 
+  dataset: 'Job',        
+  apiVersion: '2026-03-21',     
+  useCdn: false,                 
 })
